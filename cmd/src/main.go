832663902b33c3c70a1d8a09389f7cf8dab526b5
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-30 09:13:15 +0100

    added interface

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -2,8 +2,40 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreating() string
+}
+
+//the structs below have no unique fields to them but creating type englishBot and spanBot as a type struct allow
+//for the use or functions with them
+type englishBot struct{}
+
+type spanishBot struct{}
+
 func main() {
 
-	fmt.Println("whoop whoop, interfaces bitches!")
+	//interfaces have a fw functions, but one of the things interfaces can help us with is code eusibility.
 
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreating(eb)
+	printGreating(sb)
+}
+
+func printGreating(b bot) {
+
+	fmt.Println(b.getGreating())
+}
+
+//not a great deal of custom logic in the two get greting funcs but there is enough for us to understand that
+//some functions need to be defined in a custom way
+func (eb englishBot) getGreating() string {
+
+	return "Hello there you beauties"
+}
+
+func (sp spanishBot) getGreating() string {
+
+	return "Ola allí bellezas"
 }
